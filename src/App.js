457a231cc70b7d737356a1977d05da5b1cd4a37e
Petro <vankevychpetro@gmai.com>
2021-06-03 20:26:37 +0300

    forts

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@
 // import PokemonThumnail from './components/PokemonThumnail';
 import React from 'react';
 import Home from './components/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch ,useRouteMatch } from 'react-router-dom';
 import PokePage from './components/PokePage';
 import Header from './components/Header';
 
 function App() {
-
+  let match = useRouteMatch();
+    
   return (
     <Router>
     <Header/>
     <div className="app-container">
         <Switch>
           <Route path="/" component={Home} />
-          <Route exact path="/pokepage/:id" render={
+          <Route path="/" component={PokePage} />
+          <Route path={`${match.path}/:id`}>
+            <PokePage />
+        </Route>
+          {/* <Route exact path="/pokepage/:id" render={
             ({ match, location, history }) => {
               return <PokePage />
             }
-          } />
+          } /> */}
         </Switch>
     </div>
     </Router>
